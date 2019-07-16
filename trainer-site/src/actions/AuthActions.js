6d3ae518b216d0ffb600d02b2d0340223d09dc59
Papp Mihály <papp.misi@protonmail.com>
2019-07-16 21:34:54 +0200

    Fetch helyett axios

--- trainer-site/src/actions/AuthActions.js
+++ trainer-site/src/actions/AuthActions.js
@@ -1,22 +1,31 @@
+import axios from "axios";
+
 export const LOG_IN = "LOG_IN";
 export const REGISTER = "REGISTER";
 
 const address = require("../../package.json").proxy;
 
 export const logIn = userDetails => dispatch => {
-  console.log("logging in...");
-  fetch("/");
+  fetch();
 };
 
 export const register = userDetails => dispatch => {
-  fetch(`${address}/users`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(userDetails)
-  })
-    .then(res => res.json())
-    .then(res => console.log(res));
+  // fetch(`${address}/users`, {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify(userDetails)
+  // })
+  //   .then(res => res.json())
+  //   .then(res => dispatch(res));
+  console.log("jkj");
+  axios
+    .post(`${address}/users`, userDetails)
+    .then(res => {
+      console.log(res);
+      dispatch(res);
+    })
+    .catch(err => console.log(err));
 };

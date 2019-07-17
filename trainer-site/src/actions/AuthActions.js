2a984351ab33c53db33a94fac43016bb9c9091c0
Papp Mihály <papp.misi@protonmail.com>
2019-07-17 14:36:15 +0200

    Token tárolása localStorage-be

--- trainer-site/src/actions/AuthActions.js
+++ trainer-site/src/actions/AuthActions.js
@@ -8,13 +8,19 @@ const address = require("../../package.json").proxy;
 export const logIn = ({ email, password }) => dispatch => {
   axios
     .post(`${address}/auth`, {}, { auth: { username: email, password } })
-    .then(res => dispatch({ type: LOG_IN, payload: res.data }))
+    .then(({ data }) => {
+      localStorage.setItem("token", data.token);
+      dispatch({ type: LOG_IN, payload: data });
+    })
     .catch(err => console.log(err));
 };
 
 export const register = userDetails => dispatch => {
   axios
     .post(`${address}/users`, userDetails)
-    .then(res => dispatch({ type: REGISTER, payload: res.data }))
+    .then(({ data }) => {
+      localStorage.setItem("token", data.token);
+      dispatch({ type: REGISTER, payload: data });
+    })
     .catch(err => console.log(err));
 };

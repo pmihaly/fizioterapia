eb3f65b8929c9883b2cdd87fec158610d4cfcd4b
pmisi <papp.misi@protonmail.com>
2019-07-27 13:46:29 +0200

    Tornász gyakorlatainak megjelenítése

--- trainer-site/src/views/TrainingDashboard/TrainingDashboard.jsx
+++ trainer-site/src/views/TrainingDashboard/TrainingDashboard.jsx
@@ -7,6 +7,12 @@ import PropTypes from "prop-types";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getExercises } from "../../actions/ExerciseActions";
+import {
+  Button,
+  GridList,
+  GridListTile,
+  GridListTileBar
+} from "@material-ui/core";
 
 const TrainingDashboard = props => {
   useEffect(() => {
@@ -17,50 +23,30 @@ const TrainingDashboard = props => {
 
   return (
     <GridContainer direction="column" alignItems="center" justify="center">
-      <GridItem xs={12} sm={12} md={8}>
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum facilis
-          ratione consequatur perferendis consequuntur nesciunt tempore nisi?
-          Tenetur accusantium saepe deserunt asperiores! Aspernatur recusandae,
-          necessitatibus eaque id minus nulla consequuntur.
-        </h1>
-      </GridItem>
+      <GridItem xs={12} sm={12} md={8}></GridItem>
       <GridItem xs={12} sm={12} md={8}>
         <Card>
           <CardHeader color="primary">
-            <h3>Elérhető gyakorlatok</h3>
+            <h4>Elérhető gyakorlatok</h4>
           </CardHeader>
           <CardBody>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
-            aspernatur numquam quasi aliquam quaerat aut tenetur, consequuntur
-            deserunt ullam vero expedita quas debitis natus culpa nesciunt ipsa,
-            deleniti eos doloremque autem alias nihil nostrum ipsum
-            perspiciatis. Placeat eum minima obcaecati excepturi incidunt
-            repellendus ullam officiis. Obcaecati soluta perferendis eligendi
-            totam dolorum aspernatur minima repudiandae, natus sunt amet
-            temporibus iste molestias recusandae, voluptate ea blanditiis error
-            fuga laborum iure dignissimos voluptas nihil doloribus autem
-            tempora. Quo reiciendis quas, atque delectus laudantium obcaecati
-            consequuntur veniam explicabo laboriosam mollitia molestiae facilis
-            minus, ex non nostrum voluptatum dolorum assumenda vitae, illo
-            maiores tempora voluptas quia corporis quidem. Velit iusto dolor
-            laborum accusamus quidem ipsam distinctio nobis id numquam, enim sed
-            tenetur quis molestiae perspiciatis fugit quod maxime corrupti
-            labore vero exercitationem quas dolore hic sunt? Laborum consectetur
-            quo facilis assumenda aut, neque culpa ea adipisci et totam
-            aspernatur suscipit! Vitae id qui explicabo cumque soluta reiciendis
-            dolor, earum sapiente veritatis amet quisquam suscipit mollitia
-            voluptas deleniti, atque quidem perspiciatis, nisi repellat quo
-            dolorem nihil nesciunt aperiam delectus eaque. Voluptatibus numquam
-            rerum quo aut aspernatur explicabo ducimus facilis labore. Eius sed
-            in distinctio vitae obcaecati reiciendis numquam labore dolor
-            repellat quidem? Quos perferendis et quidem vitae inventore
-            accusantium ex quisquam eos doloribus tenetur. Temporibus sequi
-            labore ut quidem asperiores qui doloremque nemo nesciunt tempore
-            assumenda? Dolorum iusto soluta possimus officiis temporibus quis
-            earum eveniet rerum voluptatum blanditiis neque libero est, quaerat
-            assumenda aperiam quia eaque tempore laudantium. Beatae sapiente
-            deserunt fugit vero odio voluptatem voluptatibus?
+            <GridList
+              cols={2.5}
+              style={{
+                flexWrap: "nowrap",
+                transform: "translateZ(0)"
+              }}
+            >
+              {props.exercises.map(exercise => (
+                <GridListTile key={exercise._id}>
+                  <img
+                    src={`data:image/jpeg;base64, ${exercise.thumbnail}`}
+                    alt={`${exercise.name} gyakorlat képe`}
+                  />
+                  <GridListTileBar title={exercise.name}></GridListTileBar>
+                </GridListTile>
+              ))}
+            </GridList>
           </CardBody>
         </Card>
       </GridItem>

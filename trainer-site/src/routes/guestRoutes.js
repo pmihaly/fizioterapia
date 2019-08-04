7ef06f29a2c73e127a0d8c2fea4fbe9342b7671f
Papp Mihály <papp.misi@protonmail.com>
2019-08-04 11:24:21 +0200

    Ha be van jelentkezve a tornász, ne bejelentkezést írjon ki

--- trainer-site/src/routes/guestRoutes.js
+++ trainer-site/src/routes/guestRoutes.js
@@ -1,3 +1,4 @@
+import NavigateNext from "@material-ui/icons/NavigateNext";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import Group from "@material-ui/icons/Group";
 import Home from "@material-ui/icons/Home";
@@ -5,7 +6,7 @@ import DashboardPage from "views/Dashboard/Dashboard.jsx";
 import Authenticate from "views/Authenticate/Authenticate.jsx";
 import Icons from "views/Icons/Icons.jsx";
 
-export default [
+let routes = [
   {
     path: "főoldal",
     name: "Főoldal",
@@ -21,13 +22,26 @@ export default [
     icon: Group,
     component: Icons,
     layout: "/"
-  },
-  {
+  }
+];
+
+if (localStorage.getItem("user") === null) {
+  routes.push({
     path: "bejelentkezés",
     name: "Bejelentkezés",
     rtlName: "التطور للاحترافية",
     icon: AccountCircle,
     component: Authenticate,
     layout: "/"
-  }
-];
+  });
+} else {
+  routes.push({
+    path: "bejelentkezés",
+    name: "Tornász oldalhoz",
+    rtlName: "التطور للاحترافية",
+    icon: NavigateNext,
+    component: Authenticate,
+    layout: "/"
+  });
+}
+export default routes;
